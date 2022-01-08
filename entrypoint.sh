6cdd5ec4cefaff9f7334a9a7a4d44473e484ecb7
Dviih <dviih@dviih.email>
2022-01-08 12:59:37 -0300

    Changed the exit codes and now using cases for versions on minecraft java

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "MultiVersion | Take Off 1.0";
+echo "MultiVersion | Take Off 1.1";
 echo "Made By Dviih. https://discord.gg/Fqmhcs45pp";
 
 if [[ "$MV_FILE" == *".jar" ]]; then
@@ -11,13 +11,19 @@ if [[ "$MV_FILE" == *".jar" ]]; then
         exit 1;
     fi
 
-    if [[ "$(echo "$MC_VERSION" | cut -d "." -f 2)" -ge "$(echo "1.18" | cut -d "." -f 2)" || "$MC_VERSION" == "latest" ]]; then
-        MV_JAVA_VERSION=17
-    elif [[ "$(echo "$MC_VERSION" | cut -d "." -f 2)" -ge "$(echo "1.17" | cut -d "." -f 2)" ]]; then
-        MV_JAVA_VERSION=16
-    else
-        MV_JAVA_VERSION=8
-    fi
+    CutMC_VERSION=$(echo "$MC_VERSION" | cut -d "." -f 2)
+
+    case "$CutMC_VERSION" in
+        1[8-9]|2[0-9]|"latest")
+            MV_JAVA_VERSION=17
+            ;;
+        17)
+            MV_JAVA_VERSION=16
+            ;;
+        *)
+            MV_JAVA_VERSION=8
+            ;;
+    esac
 
     if [[ ! -f "$MV_FILE" ]]; then
         curl -sL https://mirror.dviih.tech/minecraft/"$MC_VARIANT"/"$MC_VERSION" -o "$MV_FILE";
@@ -43,7 +49,7 @@ if [[ "$MV_FILE" == *".phar" ]]; then
 
     /opt/bin/php7/bin/php --version
     /opt/bin/php7/bin/php "$MV_FILE";
-    exit 1;
+    exit 0;
 
 fi
 
@@ -57,10 +63,10 @@ if [[ "$MV_FILE" == *".js" ]]; then
         echo "NodeJS: $(node -v) [NPM: $(npm -v)]";
         node "$MV_FILE";
     else
-        echo "No such file or direcory";
+        exit 2;
     fi
 
-    exit 1;
+    exit 0;
 
 fi
 
@@ -74,10 +80,10 @@ if [[ "$MV_FILE" == *".py" ]]; then
         echo "$(python3 --version) [$(pip --version | awk '{print $1" "$2}')]";
         python3 "$MV_FILE";
     else
-        echo "No such file or direcory";
+        exit 2;
     fi
 
-    exit 1;
+    exit 0;
 
 fi
 
@@ -86,7 +92,7 @@ if [[ "$MC_VARIANT" == "bedrock_server" ]]; then
     if [[ -z "$MC_VERSION" || -z "$MV_FILE" ]]; then
         echo "Hello, You need to set the variables for Minecraft!";
         echo "Go to startup tab and change";
-        exit 1;
+        exit 11;
     fi
 
     if [[ ! -f "$MV_FILE" ]]; then
@@ -97,7 +103,7 @@ if [[ "$MC_VARIANT" == "bedrock_server" ]]; then
     fi
 
     LD_LIBRARY_PATH=. ./"$MV_FILE";
-    exit 1;
+    exit 0;
 
 fi
 
